42f7b08ec24b8d0a3ad6a35638415aa10133a536
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-01-18 04:17:15 -0300

    Finalização das telas

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
-import { StyleSheet, PermissionsAndroid, Image, View, Text } from 'react-native';
+import { StyleSheet, PermissionsAndroid, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
-import Geolocation from '@react-native-community/geolocation'
+import Geolocation from '@react-native-community/geolocation';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
 export default function Main({ navigation }) {
 
@@ -56,29 +58,43 @@ export default function Main({ navigation }) {
     return null;
 
   return (
-    <MapView
-      showsUserLocation
-      showsMyLocationButton
-      style={styles.map}
-      initialRegion={currentRegion}
-    >
-      {currentRegion &&
-        <Marker coordinate={currentRegion}>
-          <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/34848657?s=460&v=4' }} />
+    <>
+      <MapView
+        style={styles.map}
+        initialRegion={currentRegion}
+      >
+        {currentRegion &&
+          <Marker coordinate={currentRegion}>
+            <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/34848657?s=460&v=4' }} />
 
-          <Callout onPress={() => {
-            navigation.navigate('Profile', {
-              github_username: 'mateuschaves'
-            });
-          }}>
-            <View style={styles.callout}>
-              <Text style={styles.devName}>Mateus Henrique</Text>
-              <Text style={styles.devBio}>Estudante de Sistemas de Informação, desenvolvedor em um projeto da Polícia Civil e simpatizante de programação orientada a ilusões</Text>
-              <Text style={styles.devTechs}>React Native, ReactJS, Node.js</Text>
-            </View>
-          </Callout>
-        </Marker>}
-    </MapView>
+            <Callout onPress={() => {
+              navigation.navigate('Profile', {
+                github_username: 'mateuschaves'
+              });
+            }}>
+              <View style={styles.callout}>
+                <Text style={styles.devName}>Mateus Henrique</Text>
+                <Text style={styles.devBio}>Estudante de Sistemas de Informação, desenvolvedor em um projeto da Polícia Civil e simpatizante de programação orientada a ilusões</Text>
+                <Text style={styles.devTechs}>React Native, ReactJS, Node.js</Text>
+              </View>
+            </Callout>
+          </Marker>}
+      </MapView>
+
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder='Buscar devs por techs...'
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+
+        <TouchableOpacity onPress={() => { }} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -108,5 +124,38 @@ const styles = StyleSheet.create({
   },
   devTechs: {
     marginTop: 5
+  },
+  searchForm: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: "row"
+  },
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#FFF",
+    color: '#333',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 2
+  },
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#8E4Dff',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15
   }
 });
